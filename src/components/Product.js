301349166c80d6c8a997208a38ecdbbf8e6902ea
Sajith K <sajithk2k@gmail.com>
2021-09-08 18:16:44 +0530

    Initial commit

---
+++ src/components/Product.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Product extends React.Component {
+    render() { 
+        return <div>This will be the Product Page!</div>;
+    }
+}
+ 
+export default Product;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:'./src/index.js',
+    output: {
+        path: path.join(__dirname,'/dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:'babel-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+      },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template:'./src/index.html'
+        })
+    ]
+}
