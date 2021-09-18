0e8bdda007d7592553f26a258d8d2d1e218bf55f
Sajith Kumar <sajithk2k@gmail.com>
2021-09-18 14:45:28 +0530

    react router updated

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,13 +8,31 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
-  });
-
 app.get("/api/home", (req, res) => {
     res.sendFile(path.resolve(__dirname, '../', 'home_page.json'));
   });
+
+  app.get("/api/slots/:start/:end", (req,res) => { 
+    let start = req.params.start, end = req.params.end;
+    fs.readFile('../home_page.json', 'utf8' , (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      let page = [];
+      let temp = JSON.parse(data);
+      let slots = temp["RESPONSE"]["slots"];
+      for(let i=start;i<=end;i++){
+        page.push(slots[i-1]);
+      }
+      res.send(page);
+    })
+  });
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
