2726a22c64d700a20dfbc280418d2f33ed5c9267
Sajith Kumar <sajithk2k@gmail.com>
2021-09-18 11:55:57 +0530

    added pagination route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 const app = express();
 
@@ -14,6 +15,23 @@ app.get("/", (req, res) => {
 app.get("/api/home", (req, res) => {
     res.sendFile(path.resolve(__dirname, '../', 'home_page.json'));
   });
+
+  app.get("/api/slots/:start/:end", (req,res) => { 
+  let start = req.params.start, end = req.params.end;
+  fs.readFile('../home_page.json', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    let page = [];
+    let temp = JSON.parse(data);
+    let slots = temp["RESPONSE"]["slots"];
+    for(let i=start;i<=end;i++){
+      page.push(slots[i-1]);
+    }
+    res.send(page);
+  })
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
