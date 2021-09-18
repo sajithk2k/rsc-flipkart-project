8b7f61f9fa5b007162dc6c66446071fcf8981483
Divya Tyagi <divya.tyagi@BLRETV-C02FG0X7.local>
2021-09-18 16:08:49 +0530

    added shell alias

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
         alias: {
           $carousel: path.resolve(__dirname, 'src/components/Product/carousel/'),
           $details: path.resolve(__dirname, 'src/components/Product/details/'),
+          $shell: path.resolve(__dirname, 'src/components/Product/shell/'),
         },
     },
     module: {
