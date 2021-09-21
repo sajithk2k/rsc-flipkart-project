d396c2c31d5d8bd2fd8101e82fb54721d7173f82
Sajith Kumar <sajithk2k@gmail.com>
2021-09-21 12:12:00 +0530

    searchbox widget

---
+++ src/components/Home/Search/SearchBox/SearchBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchBox.css"
+export default function SearchBox(){
+    return(
+        <div className="search-box-div">
+            {/* <img src="https://static-assets-web.flixcart.com/www/linchpin/batman-returns/images/fk-default-image-75ff340b.png?q=90"></img> */}
+            <div className = "search-placeholder">Search for Products, Brands and More</div>
+        </div>
+    );
+}
